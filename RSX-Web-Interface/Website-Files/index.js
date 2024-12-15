566d4d8af4036cb564f4ce0011c0e1943da8a74b
CameronD35 <124549249+CameronD35@users.noreply.github.com>
2024-12-14 17:30:29 -0800

    Fixed index.js

--- RSX-Web-Interface/Website-Files/index.js
+++ RSX-Web-Interface/Website-Files/index.js
@@ -1,7 +1,6 @@
 import createHTMLChildElement from './modules/createElement.js';
 import SettingsOption from './modules/settingsOption.js';
 //import Graph from '/modules/lineChart.js';
-import Graph from './modules/lineChart.js';
 
 // START SETUP CODE
 
@@ -260,10 +259,6 @@ function createContentSpace(){
         }
         //createHTMLChildElement();
 
-        if(i === 0){
-            return;
-        }
-
         // Remove this guide text once you begin working on your component. 
         // After you finish your component remove the background-color from '.boxTitleContainer' and '.box' (CSS).
         let guideText = createHTMLChildElement(currentContentSpace, 'span', 'guideText', 'Place your content here.', `guideText${i}`);
@@ -275,48 +270,9 @@ function createContentSpace(){
 
 // PUT YOUR CODE HERE. FEEL FREE TO ADD, MODIFY, OR REMOVE FUNCTIONS AS NEEDED.
 
-function createComputerDataSection(container, dataTitleAndPoints, computerName){
+function createComputerDataSection(){
 
-    let computerDataContainer = createHTMLChildElement(container, 'div', 'computerDataContainer');
-
-    let computerNameSection = createHTMLChildElement(computerDataContainer, 'div', 'computerNameSection', computerName);
-    
-    let border = createHTMLChildElement(computerDataContainer, 'div', 'border', null, `computerDataBorder`)
-
-    for(let i = 0; i < dataTitleAndPoints.length; i++){
-        let currentDatumSection = createHTMLChildElement(computerDataContainer, 'div', 'computerDatumSection', null, `${dataTitleAndPoints[i].title.substring(0,3)}ComputerDatumSection`);
-
-        let currentDatumTitle = createHTMLChildElement(currentDatumSection, 'div', `computerDataTitle`, `${dataTitleAndPoints[i].title}:`, `${dataTitleAndPoints[i].title.substring(0,3)}ComputerDataTitle`);
-
-        let currentDatumPoint = createHTMLChildElement(currentDatumSection, 'span', `computerDataPoint`, dataTitleAndPoints[i].data, `${dataTitleAndPoints[i].title.substring(0,3)}ComputerDataPoint`);
-
-        if(i == dataTitleAndPoints.length - 1){
-            continue;
-        }
-    }
 }
-
-createComputerDataSection(document.getElementById('contentContainer0'), [
-    {
-        title: 'RAM',
-        data: '16GB'
-    },
-
-    {
-        title: 'IP Address',
-        data: '0000x6F'
-    },
-
-    {
-        title: 'Currently Running',
-        data: 'cv.py'
-    },
-
-    {
-        title: 'Something Else',
-        data: 'Cool'
-    }
-], 'Hal-3000')
 
 // Creates the button/icon to open the settings
 function createSettingsBox(parent){
@@ -325,6 +281,16 @@ function createSettingsBox(parent){
     settingsButton.src = './Image-Assets/SettingsIcon.webp';
 
     settingsButton.addEventListener("click", settingsButtonClicked);
+
+
+    /** Open settings when button clicked. */
+    function settingsButtonClicked()
+    {  
+        showOverlay(createSettingsSection);
+        //closeButton.addEventListener("click", closeSettingsButtonClicked);
+        document.getElementById('settingsDialog').show();
+    }
+}
 
 // Creates the content when the settings icon is clicked
 function createSettingsSection(settingsUIContainer=document.getElementById('overlayContentContainer')){
@@ -346,7 +312,6 @@ function createSettingsSection(settingsUIContainer=document.getElementById('over
 
     // Listen for user clicking the button.
     closeButton.addEventListener('click', closeSettingsButtonClicked)
-    
 
 
     /* When the close button on the settings panel is clicked. */
