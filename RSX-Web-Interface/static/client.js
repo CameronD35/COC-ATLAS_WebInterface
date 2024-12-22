3f240f212af1da777705f9a5a271b7c4e4d29c1a
CameronD35 <camster09@ca.rr.com>
2024-12-22 00:11:56 -0800

    Removed MORE unecessary code.

--- RSX-Web-Interface/static/client.js
+++ RSX-Web-Interface/static/client.js
@@ -1,13 +1,2 @@
+// Global io variable. It's initialization sends message to server that a client has connected.
 const socket = io();
-
-const messageInput = document.querySelector('.messageInput');
-const sendButton = document.querySelector('.sendButton');
-const messagesContainer = document.querySelector('.messagesContainer');
-
-socket.on('chat message', (msg) => {
-    const message = document.createElement('div');
-    message.classList.add('message');
-    message.textContent = msg;
-    messagesContainer.appendChild(message);
-    window.scrollTo(0, document.body.scrollHeight);
-});
